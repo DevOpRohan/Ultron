bb5208bd1777c794db0b3cadbc0576dd4c916098
Rohan Verma <86923317+DevOpRohan@users.noreply.github.com>
2023-04-16 09:19:11 +0530

    Update Chat.js
    Remove commented code

--- src/Chat.js
+++ src/Chat.js
@@ -63,56 +63,6 @@ const Chat = () => {
     }
   }, [chatMessages]);
 
-  // const formatMessage = (message) => {
-  //   return message.split("```").map((snippet, index) => {
-  //     if (index % 2 === 1) {
-  //       const languageMatch = snippet.match(/^(\w+)\n/);
-  //       const language = languageMatch ? languageMatch[1] : null;
-  //       const cleanedSnippet = snippet.replace(/^\w+\n/, "");
-  //       const highlightedSnippet = hljs.highlightAuto(
-  //         cleanedSnippet,
-  //         language ? [language] : null
-  //       ).value;
-  //       return (
-  //         <div
-  //           key={index}
-  //           style={{
-  //             position: "relative",
-  //             backgroundColor: "#f1f1f1",
-  //             border: "1px solid #ccc",
-  //             borderRadius: "3px",
-  //             padding: "8px",
-  //             marginBottom: "8px"
-  //           }}
-  //         >
-  //           <pre
-  //             style={{
-  //               overflowX: "auto",
-  //               whiteSpace: "pre-wrap",
-  //               wordWrap: "break-word"
-  //             }}
-  //             dangerouslySetInnerHTML={{ __html: highlightedSnippet }}
-  //           ></pre>
-  //           <FiCopy
-  //             style={{
-  //               cursor: "pointer",
-  //               position: "absolute",
-  //               right: "8px",
-  //               top: "8px",
-  //               color: "#0078d4",
-  //               fontSize: "1.2rem"
-  //             }}
-  //             onClick={() => {
-  //               navigator.clipboard.writeText(cleanedSnippet);
-  //             }}
-  //           />
-  //         </div>
-  //       );
-  //     } else {
-  //       return <span key={index}>{snippet.replace(/\n/g, <br />)}</span>;
-  //     }
-  //   });
-  // };
   const formatMessage = (message) => {
     return message.split("```").map((snippet, index) => {
       if (index % 2 === 1) {
